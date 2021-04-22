3bc46c42e63b248fc424a7d1b33d3b53d3ee8824
mirkoh007 <mirkoh007@gmail.com>
2021-04-23 00:19:39 +0200

    pocetak rada na aplikaciji. napravnljena konekcija.

--- src/com/mirkoh/Application.java
+++ src/com/mirkoh/Application.java
@@ -1,9 +1,29 @@
 package com.mirkoh;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Application {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello");
+    public static Connection conn;
+    private static final String studentskasluzba = "jdbc:mysql://localhost:3306/studentskasluzba";
+    private static final String username = "root";
+    private static final String password = "root";
+
+    static {
+        try {
+            conn = DriverManager.getConnection(studentskasluzba,username,password);
+
+        }catch (SQLException e) {
+            System.out.println("Nesto je otislo po zlu");
+            e.printStackTrace();
+        }
     }
+
+    public static void main(String[] args) {
+
+
+    }
+
 }
